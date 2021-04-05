b824c23f7c89cd8dc6c35e04c9364866f01993ef
wahomeJohn <66598945+wahomeJohn@users.noreply.github.com>
2021-04-05 14:36:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Myfirst program
+#Enter the first and last name
+
+names = input("Enter first and last name :")
+print(names)
+print()
+
+#goals for the class
+print("what are the goals for the class?")
+goals = input("what would you like to be? :")
+print (goals)
